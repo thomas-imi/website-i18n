ba1de84d4a6303a921df20be2950c353b50e2c0c
e271828- <e271828-@users.noreply.github.com>
2021-03-19 15:27:34 -0700

    switch to Github Actions. closes #14

---
+++ bin/check_expected_keys.py
@@ -0,0 +1,44 @@
+"""
+Compares translation_key.json to a specific language file to see
+if either values are missing or superfluous keys are included.
+
+Args: file_to_check.json
+"""
+import json
+import sys
+
+def NestedDictValues(d):
+  for v in d.values():
+    if isinstance(v, dict):
+      yield from NestedDictValues(v)
+    else:
+      yield v
+
+def NestedDictKeys(d):
+  for k in d.keys():
+    if isinstance(k, dict):
+      yield from NestedDictKeys(k)
+    else:
+      yield k
+
+with open('src/translation_key.json', encoding='utf-8') as fh:
+    data = json.load(fh)
+
+to_check = sys.argv[1]
+
+with open(to_check, encoding='utf-8') as fh:
+    data2 = json.load(fh)
+
+wanted = set(list(NestedDictKeys(data)))
+found = set(list(NestedDictKeys(data2)))
+
+diff = wanted.difference(found)
+
+wanted_len = len(wanted)
+found_len = len(found)
+
+if diff:
+    print(f"{to_check}: wanted {wanted_len} found {found_len}")
+    print(diff)
+    sys.exit(1)
+
--- tests/validate.sh
+++ tests/validate.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-find . -not -path './node_modules/*' -type f -name '*.json' | while read json; do echo $json ; jq --exit-status . $json > /dev/null || exit 1; done
+# basic validity checks
+echo "checking JSON validity"
+find . -not -path './node_modules/*' -not  -path './package*' -type f -name '*.json' | while read json; do echo $json ; jq --exit-status . $json > /dev/null || exit 1; done
+
+# check for HTML entities
+echo "checking for HTML entities"
+grep -F '&#' *.json && exit 1
+
+# key mismatch checks
+echo "checking for mismatched keysets, i.e. missing or superfluous keys"
+find . -type f  -iname "*.json" -not -path './src/*' | while read json; do echo $json ; python3 bin/check_expected_keys.py $json || exit 1; done
