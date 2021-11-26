c5219ad4e659fba67a8010781d36d5df82bd3431
e271828- <e271828-@users.noreply.github.com>
2021-11-25 16:36:06 -0800

    update all copy

---
+++ scripts/fetch_keys.py
@@ -0,0 +1,51 @@
+"""
+Fetches all pages in sitemap, extracts translation key pairs.
+
+Usage:
+
+./fetch_keys.py "https://site/sitemap.xml"
+"""
+
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+import requests
+import json
+import sys
+
+sitemap_uri = sys.argv[1]
+
+def parser_sitemap_xml(sitemap_link):
+    response = requests.get(sitemap_link).text
+    soup = BeautifulSoup(response, 'html.parser')
+    urls = soup.find_all('loc')
+    list = []
+    for url in urls:
+        url = str(url)
+        url = url.replace("<loc>", "").replace("</loc>", "")
+        list.append(url)
+    return list
+
+
+def run(playwright, url):
+    webkit = playwright.webkit
+    browser = webkit.launch()
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto(url)
+    result = page.evaluate("JSON.stringify(tKeys)")
+    browser.close()
+    return result
+
+array = parser_sitemap_xml(sitemap_uri)
+
+deduped_dict = {}
+
+with sync_playwright() as playwright:
+    for url in array:
+        r = run(playwright, url)
+        j = json.loads(r)
+        for k in j.keys():
+            if k not in deduped_dict:
+                deduped_dict[k] = j[k]
+
+print(json.dumps(deduped_dict))
